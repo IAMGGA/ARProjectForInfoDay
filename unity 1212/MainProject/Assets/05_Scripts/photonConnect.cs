ce6eeb8a04d92ebf629d7a8b65fe6057c07482b8
IAMGGA <44642710+IAMGGA@users.noreply.github.com>
2018-11-22 01:38:32 +0800

    Making Photon
    making photon lobby

---
+++ unity 1212/MainProject/Assets/05_Scripts/photonConnect.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class photonConnect : MonoBehaviour {
+
+    public string versionName = "0.1";
+
+    public GameObject StartMenu, LobbyMenu, DisconnectedMenu;
+
+
+    public void StartConnect()
+    {
+        PhotonNetwork.ConnectUsingSettings(versionName);
+
+        Debug.Log("Connecting to photon...");
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+
+        Debug.Log("We are connected to master");
+    }
+
+    private void OnJoinedLobby()
+    {
+        StartMenu.SetActive(false);
+        LobbyMenu.SetActive(true);
+
+        Debug.Log("On Joined Lobby");
+    }
+
+    private void OnFailedToConnectToPhoton()
+    {
+        if (StartMenu.active)
+            StartMenu.SetActive(false);
+
+        if (LobbyMenu.active)
+            LobbyMenu.SetActive(false);
+
+        DisconnectedMenu.SetActive(true);
+
+        Debug.Log("Disconnect from photon services");
+
+    }
+
+
+   
+}
---
+++ unity 1212/MainProject/Assets/05_Scripts/photonHandlerNoDestroy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class photonHandlerNoDestroy : MonoBehaviour {
+
+    public InputField createInput, joinInput;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.transform);
+        SceneManager.sceneLoaded += OnSceneFinishedLoading;
+
+    }
+
+    public void onClickCreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text, new RoomOptions() { MaxPlayers = 2 }, null);
+
+    }
+
+    public void onClickJoinRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.maxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom(joinInput.text, roomOptions, TypedLobby.Default);
+
+    }
+
+	private void moveGameScene()
+    {
+        PhotonNetwork.LoadLevel("Character2");
+    }
+
+    private void OnJoinedRoom() {
+        moveGameScene();
+        Debug.Log("We are connected to the room");
+
+    }
+
+    private void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name == "Character2")
+        {
+            
+        }
+    }
+
+}
